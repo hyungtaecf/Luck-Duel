61c614f52cc233fbaf79ab5bad02b1a1f67c33be
606-15 <a606-15@606-15ui-iMac.local>
2017-11-03 19:50:12 +0900

    Initial Commit

---
+++ Luck Duel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Luck Duel
+//
+//  Created by 606-15 on 2017. 11. 3..
+//  Copyright © 2017년 606-15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
